80b3df83ec91463fc15a35dffa123101c944329c
준 강 <junijuniya@naver.com>
2020-08-27 22:33:22 +0900

    초기 커밋.

---
+++ FontXam/FontXam/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("Typo_EoulrimL.ttf", Alias = "RegularFont")]
+[assembly: ExportFont("Typo_EoulrimM.ttf", Alias = "MediumFont")]
+[assembly: ExportFont("Typo_EoulrimB.ttf", Alias = "BoldFont")]
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace FontXam
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
